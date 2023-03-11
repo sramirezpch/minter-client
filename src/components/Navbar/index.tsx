b11d86997e9a4ad24bb7e5283c1c02073eec5ea9
Sergio Ramirez <sergio@openloophealth.com>
2023-03-11 00:12:50 -0500

    chore: improving code and styles

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,24 +1,26 @@
-import Link from "next/link";
 import { useAppSelector } from "../../hooks";
+import WalletButton from "../WalletButton";
 
 import styles from "./Navbar.module.css";
 
-export default function () {
+const Navbar = () => {
   const account = useAppSelector((state) => state.account.address);
 
   return (
     <nav
-      className="bg-slate-500 flex items-center justify-between p-3"
-      style={{ height: "64px", backgroundColor: "rgb(113 113 122)" }}
+      className="bg-slate-500 p-3"
+      style={{
+        height: "64px",
+        backgroundColor: "rgb(113 113 122)",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
     >
-      <div>
-        <Link href="/" className={styles.logo}>
-          NFT Minter
-        </Link>
-      </div>
-      <div className="ms-auto">
-        <span>{account ? account : "No wallet connected"}</span>
-      </div>
+      <div className={styles.logo}>NFT Minter</div>
+      <span>{account ? account : <WalletButton />}</span>
     </nav>
   );
-}
+};
+
+export default Navbar;
