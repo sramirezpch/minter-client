7d0cd4957246b0984ae5f25c3d4bc535dc230e8d
Sergio Ramirez <sergio@openloophealth.com>
2023-03-21 17:53:29 -0500

    chore: modified code

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,9 +1,4 @@
 import { ethers } from "ethers";
-import { Alchemy } from "alchemy-sdk";
-
-export interface IWrapped {
-  alchemy?: Alchemy;
-}
 
 export interface PinFilAxiosResponse {
   hash: string;
