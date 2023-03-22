a7519be21059262a89ec5f784cd0396a98a7bdc5
Sergio Ramirez <sergio@openloophealth.com>
2023-03-22 00:38:37 -0500

    chore: remove collisioned code

--- src/hocs/withProvider.tsx
+++ src/hocs/withProvider.tsx
@@ -2,13 +2,12 @@ import { FC, useEffect, useState } from "react";
 import Web3Context from "../context/web3.context";
 
 import { useAppDispatch, useWeb3Tools } from "../hooks";
-import { IWrapped } from "../interfaces";
 
 import { updateAccount } from "../redux/slices/account.reducer";
 
-export const withProvider = (WrappedComponent: FC<IWrapped>) => {
+export const withProvider = (WrappedComponent: FC) => {
   const EnhancedComponent: FC = (props) => {
-    const { provider, alchemy } = useWeb3Tools();
+    const { provider } = useWeb3Tools();
 
     const dispatch = useAppDispatch();
 
@@ -33,7 +32,7 @@ export const withProvider = (WrappedComponent: FC<IWrapped>) => {
 
     return (
       <Web3Context.Provider value={provider}>
-        <WrappedComponent {...props} alchemy={alchemy} />
+        <WrappedComponent {...props} />
       </Web3Context.Provider>
     );
   };
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,3 @@
-import { ethers } from "ethers";
-
 export interface PinFilAxiosResponse {
   hash: string;
   message: string;
